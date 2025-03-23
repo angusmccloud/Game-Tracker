9cabda6cb0f474b2f5877efc7c35599caeb1a144
Connor Tyrrell <connor@brewgene.com>
2025-03-22 18:07:58 -0600

    If you're in an active game, let you rejoin

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,29 @@
 'use client'; // directive for Next.js to indicate this is a client-side component
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PageHeader from '@/app/containers/PageHeader/PageHeader';
 import PageWrapper from '@/app/containers/PageWrapper/PageWrapper';
 import JoinGameModal from '@/app/containers/JoinGameModal/JoinGameModal';
 import Button from '@/app/components/Button/Button';
+import { getCurrentGame } from '@/app/utils/localStorage';
+import { getGameStatus } from '@/app/service/games';
 
 // Main component for the HomePage
 export default function HomePage() {
   const [showJoinGameModal, setShowJoinGameModal] = useState(false);
+  const [rejoinGame, setRejoinGame] = useState(false);
+
+  useEffect(() => {
+    const checkRejoinGame = async () => {
+      const currentGame = await getCurrentGame();
+      if (currentGame) {
+        const { success, gameStatus } = await getGameStatus(currentGame.gameId);
+        if (success && (gameStatus === 'setup' || gameStatus === 'inProgress')) {
+          setRejoinGame(true);
+        }
+      }
+    };
+    checkRejoinGame();
+  }, []);
 
   return (
     <>
@@ -20,6 +36,11 @@ export default function HomePage() {
         <Button variant="contained" onClick={() => setShowJoinGameModal(true)}>
           Join Game
         </Button>
+        {rejoinGame && (
+          <Button variant="contained" onClick={() => window.location.href = '/play'}>
+            Rejoin Game
+          </Button>
+        )}
       </PageWrapper>
     </>
   );
